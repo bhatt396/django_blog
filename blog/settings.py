66f17353ad9ddeaf655c09b3326baa9c993ae59b
Bhaskar <079bei013.bhaskar@pcampus.edu.np>
2025-05-26 16:04:13 +0545

    Fix template static tag and update views, remove unused template

--- blog/settings.py
+++ blog/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # <--- Add the forward slashes correctly
+
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
