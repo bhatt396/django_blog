cb46b7e03f96e3971161993882645afcdf6201b6
Bhaskar <079bei013.bhaskar@pcampus.edu.np>
2025-05-26 16:08:25 +0545

    Fix static tag syntax and update templates, views

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['crud/templates/**/*.html'],  // ✅ Path to your Django templates
+    theme: {
+      extend: {},                           // ✅ No typo here
+    },
+    plugins: [],                             // ✅ Plugins array is fine
+  }
+  
